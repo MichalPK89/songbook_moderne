2bac294f7000d597c560e92cf603d54e74632772
MichalPK89 <miskomiskopk@gmail.com>
2026-04-12 20:42:53 +0200

    Update transposer.js

--- js/transposer.js
+++ js/transposer.js
@@ -1,9 +1,1 @@
-document.write('\
-\
-    <h1>Add your HTML code here</h1>\
-\
-     <p>Notice however, that you have to escape LF's with a '\', just like\
-        demonstrated in this code listing.\
-    </p>\
-\
-');
+document.write('<center><footer>michalmiskovic89@gmail.com</footer></center>')
