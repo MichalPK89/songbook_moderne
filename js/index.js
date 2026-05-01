87d593ae9d798f4e56ddbd64cbe7ae55f18f842b
MichalPK89 <miskomiskopk@gmail.com>
2026-05-01 20:14:30 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,47 @@
 function createSongList() {
-  let text = "";  
-  for (song in songs) {
-      text += songs[song].title
-      text += songs[song].performer
-      text += songs[song].author
-      text += songs[song].genre
-      }
+  let list = "";
+  var x = document.createElement("TABLE");
+  x.setAttribute("id", "index");
+ 
+  var tbody = document.createElement("tbody");
+  x.appendChild(tbody);
+  document.body.appendChild(x);
   
-  document.getElementById("list").innerHTML = text;  
-}
+  /*
+  var row = document.createElement("TR");
+  var cell = document.createElement("TH");
+  var text = "Názov";
+  cell.appendChild(text);
+  row.appendChild(cell);
+  x.appendChild(row);
+  */
+  for (song in songs) {
+		
+		var row = document.createElement("TR");
+		
+		var cell = document.createElement("TD");
+		var text = document.createTextNode(songs[song].title);
+		cell.appendChild(text);
+		row.appendChild(cell);
+		
+		var cell = document.createElement("TD");
+		var text = document.createTextNode(songs[song].performer);
+		cell.appendChild(text);
+		row.appendChild(cell);
+		
+		var cell = document.createElement("TD");
+		var text = document.createTextNode(songs[song].author);
+		cell.appendChild(text);
+		row.appendChild(cell);
+		
+		var cell = document.createElement("TD");
+		var text = document.createTextNode(songs[song].genre);
+		cell.appendChild(text);
+		row.appendChild(cell);
+		
+		x.appendChild(row);
+      }
 
+  
+  document.getElementById("list").innerHTML = list;  
+}
