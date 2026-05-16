fa1d9377818b506538a7854ec94d3fb82023395b
MichalPK89 <miskomiskopk@gmail.com>
2026-05-16 18:56:48 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -66,3 +66,22 @@ function createSongList() {
   
   document.getElementById("list").innerHTML = list;  
 }
+
+function filterTable() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("filter");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("index");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
