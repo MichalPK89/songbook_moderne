cec3c02c92761839cf97104a0c81e63d37165600
MichalPK89 <miskomiskopk@gmail.com>
2026-05-19 18:23:18 +0200

    Update list_moderne.js

--- js/list_moderne.js
+++ js/list_moderne.js
@@ -22,6 +22,16 @@ const songs = {
     genre: "folk",
     language: "cz",
     video: "https://www.youtube.com/watch?v=0yD07CFelIs"
+  },
+
+  ty_jsi_ten_dest: {
+    title: "Ty jsi ten déšť (muzikál Johanka z Arku)",
+    performer: "Petr Kolář/Kamil Střihavka/Lucie Bílá",
+    music: "Ondřej Soukup",
+    lyrics: "Gabriela Osvaldová",
+    genre: "rock",
+    language: "cz",
+    video: "https://www.youtube.com/watch?v=vekOu1nmKi4&t=22s"
   }
   
 }
